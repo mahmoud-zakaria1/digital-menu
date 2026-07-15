e0bdf77b821957c8f23c0351a09d9289273fe949
Mahmoud Zakaria <mz2862001@gmail.com>
2026-07-15 04:17:59 +0300

    refactor: architecture improvements, type-safe config, and centralized 404 error routing

--- Backend-POS/src/app.ts
+++ Backend-POS/src/app.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import { connectDB } from "./config/db.js";
-import  config  from "./config/config.js";
+import config from "./config/config.js";
 import globalErrorHandling from "./middlewares/globalErrorHandler.js";
 // import createHttpError from "http-errors";
 const app = express();
@@ -10,12 +10,10 @@ connectDB();
 
 app.use(express.json());
 
-
 app.use((req: Request, res: Response, next: NextFunction) => {
-
   // const err = createHttpError(404, "Something went wrong");
   // throw err;
-  const error: any = new Error (`Route ${req.originalUrl} not found`)
+  const error: any = new Error(`Route ${req.originalUrl} not found`);
   error.statusCode = 404;
   next(error);
 });
--- Backend-POS/src/config/config.ts
+++ Backend-POS/src/config/config.ts
@@ -2,15 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 interface Config {
-    port: number;
-    databaseURI: string;
-    nodeEnv: "development" | "production" | "test";
+  port: number;
+  databaseURI: string;
+  nodeEnv: "development" | "production" | "test";
 }
 
 const config = Object.freeze({
-    port: Number(process.env.PORT) || 3000,
-    databaseURI: process.env.MONGODB_URI || "mongodb://localhost:2701",
-    nodeEnv: process.env.NODE_ENV || "development"
+  port: Number(process.env.PORT) || 3000,
+  databaseURI: process.env.MONGODB_URI || "mongodb://localhost:2701",
+  nodeEnv: process.env.NODE_ENV || "development",
 });
 
 export default config;
