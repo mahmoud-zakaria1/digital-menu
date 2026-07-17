45cf9fbabf1093c461e717184638cc62271d2899
Mahmoud Zakaria <mz2862001@gmail.com>
2026-07-17 20:24:18 +0300

    refactor: reordering middlewares in app.ts to fix 404 routing issue

--- Backend-POS/src/app.ts
+++ Backend-POS/src/app.ts
@@ -2,25 +2,25 @@ import express, { NextFunction, Request, Response } from "express";
 import { connectDB } from "./config/db.js";
 import config from "./config/config.js";
 import globalErrorHandling from "./middlewares/globalErrorHandler.js";
-// import createHttpError from "http-errors";
+import userRoute from "./routes/users.route.js";
+
 const app = express();
 
-const PORT = config.port;
+const PORT = config.port || 8000;
+
 connectDB();
 
 app.use(express.json());
 
+app.use("/api/users", userRoute);
+
 app.use((req: Request, res: Response, next: NextFunction) => {
-  // const err = createHttpError(404, "Something went wrong");
-  // throw err;
   const error: any = new Error(`Route ${req.originalUrl} not found`);
   error.statusCode = 404;
   next(error);
 });
 
 app.use(globalErrorHandling);
-
-// const port = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
