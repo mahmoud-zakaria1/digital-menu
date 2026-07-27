a609c8602ec6081d5b7714214e5eec09180d587c
Mahmoud Zakaria <mz2862001@gmail.com>
2026-07-27 15:46:09 +0300

    feat: add reservedAt field to table schema and types

---
+++ Backend-POS/src/models/table.schema.ts
@@ -0,0 +1,25 @@
+import { ITable } from '../types/table.types.js';
+import mongoose, {Schema, Model} from "mongoose";
+
+const tableSchema = new Schema<ITable> (
+    {
+        tableNo: {
+            type: Number,
+            required: [true, "Table number is required"],
+            unique: true,
+        },
+        status: {
+            type: String,
+            enum: ["Available", "Occupied", "Reserved"],
+            default: "Available",
+        },
+        currentOrder: {
+            type: Schema.Types.ObjectId,
+            ref: "Order",
+        },
+    },
+    {timestamps: true},
+);
+
+const Table: Model<ITable> = mongoose.model<ITable>("Tabel", tableSchema);
+export default Table;
---
+++ Backend-POS/src/types/table.types.ts
@@ -0,0 +1,12 @@
+import { Document, Types } from "mongoose";
+
+export type TableStatus = "Available" | "Occupied" | "Reserved" ;
+
+export interface ITableFields {
+    tableNo: number,
+    status: TableStatus,
+    currentOrder?: Types.ObjectId;
+    reservedAt?: Date;
+}
+
+export interface ITable extends ITableFields, Document {}
