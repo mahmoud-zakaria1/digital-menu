63186878dd8a9625ce2342832cebf945604a66f5
Mahmoud Zakaria <mz2862001@gmail.com>
2026-07-29 22:48:39 +0300

    fix: export router instance instead of Router class (fixes hanging requests), fix deleteMeal registered on wrong router, add missing :id param to update/delete routes

--- Backend-POS/src/routes/meals.route.ts
+++ Backend-POS/src/routes/meals.route.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
 import { createMeal, getAllMeals, getMealById, updateMeal, deleteMeal } from '../controllers/meals.controller.js';
 import { isVerifiedUser, isAdmin } from "../middlewares/tokenVerfication.js";
-import route from "./users.route.js";
 
 const router = Router();
 
+router.post("/createMeal", isVerifiedUser, isAdmin, createMeal);
 router.get("/", getAllMeals);
 router.get("/:id", getMealById);
+router.put("/updateMeal/:id", isVerifiedUser, isAdmin, updateMeal);
+router.delete("/deleteMeal/:id", isVerifiedUser, isAdmin, deleteMeal);
 
-router.post("/createMeal", isVerifiedUser, isAdmin, createMeal);
-router.put("/updateMeal", isVerifiedUser, isAdmin, updateMeal);
-route.delete("/deleteMeal", isVerifiedUser, isAdmin, deleteMeal);
-
-export default Router;
+export default router;
