e3eace56a2216ba8ca5b2c054c81bf1607316276
Mahmoud Zakaria <mz2862001@gmail.com>
2026-08-24 03:48:57 +0300

    chore: configure Jest with ts-jest ESM preset and supertest for API integration testing

---
+++ Backend-POS/jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
+  },
+  testMatch: ["**/*.test.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setup.ts"],
+  testTimeout: 20000,
+};
